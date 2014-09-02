9aee320b971e1cc1bebfdfde558688bb250dd9e4
Andrey Epifanov <aepifanov@mirantis.com>
2014-09-02 23:18:34 +0200

    Add arg_parser, checking options and confirmation for cleanup.

--- functions.sh
+++ functions.sh
@@ -1,27 +1,48 @@
 #!/bin/bash
+function create_disk {
 
+    local _NAME=${1:-NAME}
+    local _DISK=${2:-DISK}
+
+    echo "Creating storage..."
+    echo -e "Name: \"${_NAME}\"\nSize: ${_DISK}"
+
+    virsh vol-create-as --name $_NAME.qcow2 --capacity $_DISK --format qcow2 --allocation $_DISK --pool default
+}
 
 function setup_network {
-        VLAN=$1
-        STR=$2
-	virsh dumpxml $name > $name.xml
-	sed "0,/network='internal'\\/>/s//network='internal' portgroup='vlan-$VLAN'\\/>/" -i $name.xml
+
+
+    local _NAME=${1:-NAME}
+    local _VLAN=${2:-VLAN}
+
+    echo "Setup network..."
+    echo -e "Name: \"${_NAME}\"\nVLAN ID: ${_VLAN}"
+
+    virsh dumpxml ${_NAME} > ${_NAME}.xml
+	sed "0,/network='internal'\\/>/s//network='internal' portgroup='vlan-${_VLAN}'\\/>/" -i ${_NAME}.xml
 #        awk '{print} NR==$STR {while (getline < "vlan.xml") print}' $name.xml > $name_new.xml
-	virsh define $name.xml
-        rm -f $name.xml
+	virsh define ${_NAME}.xml
+    rm -f ${_NAME}.xml
 }
 
 
 function setup_iso {
+
+    local _NAME=${1:-NAME}
+    local _GW=${2:-GATEWAY_IP}
+
+    echo "Setup ISO..."
+
 	TMPD=$(mktemp -d)
-        IMAGE_PATH=/var/lib/libvirt/images
-	modprobe nbd max_part=63                                                        
-	qemu-nbd -n -c /dev/nbd0 $IMAGE_PATH/$name.qcow2
+    IMAGE_PATH=/var/lib/libvirt/images
+	modprobe nbd max_part=63
+	qemu-nbd -n -c /dev/nbd0 $IMAGE_PATH/${_NAME}.qcow2
 	vgscan --mknode
 	vgchange -ay os
 	mount /dev/os/root $TMPD
-	sed "s/GATEWAY=.*/GATEWAY=\"$gateway_ip\"/g" -i $TMPD/etc/sysconfig/network
-        echo "
+	sed "s/GATEWAY=.*/GATEWAY=\"${_GW}\"/g" -i $TMPD/etc/sysconfig/network
+    echo "
 DEVICE=eth1
 TYPE=Ethernet
 ONBOOT=yes
